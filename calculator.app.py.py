59b757bb3d5837f0b855cdd125d0e06ff697020f
VASUMATHI M <mvasumathi15@gmail.com>
2026-03-15 22:55:57 +0530

    Add files via upload

---
+++ calculator.app.py.py
@@ -0,0 +1,25 @@
+# Simple Python Calculator
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+op = input("Enter operation (+, -, *, /): ")
+
+if op == "+":
+    print("Result:", num1 + num2)
+
+elif op == "-":
+    print("Result:", num1 - num2)
+
+elif op == "*":
+    print("Result:", num1 * num2)
+
+elif op == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Cannot divide by zero")
+
+else:
+    print("Invalid operator")
+input("Press Enter to exit...")
